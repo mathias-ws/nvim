3e69267705d35dbcb8183da40e66dfa6c6b8c0b4
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2023-08-25 10:03:44 +0200

    Added status line

--- lua/mws/init.lua
+++ lua/mws/init.lua
@@ -1,6 +1,6 @@
 require("mws.remap")
 require("mws.basic_config")
-
+require("mws.statusline").setup()
 --require("mws.venv")
 
 require("mws.lazy")
---
+++ lua/mws/statusline.lua
@@ -0,0 +1,45 @@
+-- Gets the git branch
+local function git_branch()
+    local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
+    if string.len(branch) > 0 then
+        return branch
+    else
+        return ":"
+    end
+end
+
+local M = {}
+
+M.setup = function()
+    local set_color_1 = "%#PmenuSel#"
+    local branch = git_branch()
+    local set_color_2 = "%#LineNr#"
+    local file_name = " %f"
+    local modified = "%m"
+    local align_right = "%="
+    local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
+    local fileformat = " [%{&fileformat}]"
+    local filetype = " %y"
+    local percentage = " %p%%"
+    local linecol = " %l:%c"
+
+    local status_line = string.format(
+    "%s %s %s%s%s%s%s%s%s%s%s",
+    set_color_1,
+    branch,
+    set_color_2,
+    file_name,
+    modified,
+    align_right,
+    filetype,
+    fileencoding,
+    fileformat,
+    percentage,
+    linecol
+    )
+
+  vim.opt.statusline = status_line
+end
+
+return M
+
