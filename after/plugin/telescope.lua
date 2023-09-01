f014929443d7ac7cb2dd74ff85734231df5997cb
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2023-09-01 20:25:52 +0200

    Moved all telescope settings into after.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,23 @@
 local builtin = require('telescope.builtin')
+
+-- Setting keymaps.
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- Search through help tags.
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = { "venv", "virt_env", "env", ".idea", ".git" }
+    },
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+})
 
