9b3b314006c21eea9bef26b03560f985106dcb78
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2023-10-01 13:50:26 +0200

    Formatting + now using livegrep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,23 +1,22 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 -- Setting keymaps.
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  require("telescope.builtin").live_grep()
 end)
 
 -- Search through help tags.
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 
 require("telescope").setup({
-    defaults = {
-        file_ignore_patterns = { "venv", "virt_env", "env", ".idea", ".git" }
+  defaults = {
+    file_ignore_patterns = { "venv", "virt_env", "env", ".idea", ".git" },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
     },
-    pickers = {
-        find_files = {
-            hidden = true
-        }
-    }
+  },
 })
-
