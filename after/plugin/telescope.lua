0e8a13ccfe87a0b17febe511da6a82cd6b666aee
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2023-10-01 13:52:06 +0200

    Renamed variable.

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,14 @@
-local builtin = require("telescope.builtin")
+local telescope = require("telescope.builtin")
 
 -- Setting keymaps.
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>pf", telescope.find_files, {})
+vim.keymap.set("n", "<C-p>", telescope.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
-  require("telescope.builtin").live_grep()
+  telescope.live_grep()
 end)
 
 -- Search through help tags.
-vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>vh", telescope.help_tags, {})
 
 require("telescope").setup({
   defaults = {
