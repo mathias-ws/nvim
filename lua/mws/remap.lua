9d4b9623df5e30a10ce3893ac9b3b7420e54e13c
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2023-10-01 14:11:03 +0200

    Added keybinds for adding block comments in python and yaml.

--- lua/mws/remap.lua
+++ lua/mws/remap.lua
@@ -9,3 +9,13 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Exit terminal mode with esc
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
+
+-- Add comments on selected lines in visual mode
+vim.cmd([[
+  autocmd FileType python,yaml vnoremap <buffer><leader>c :s/^/# /<CR>
+]])
+
+-- Add comments to all lines of a file
+vim.cmd([[
+  autocmd FileType python,yaml nnoremap <buffer><leader>C :%s/^/# /<CR>
+]])
