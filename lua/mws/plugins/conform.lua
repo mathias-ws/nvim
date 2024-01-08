dc3e46fa7ef1714fa928fbef588a7f852b633201
Mathias Westby Skoglund <work@mathiaswskoglund.com>
2024-01-08 09:23:20 +0100

    Removed lua lsp and formatter from work config.

--- lua/mws/plugins/conform.lua
+++ lua/mws/plugins/conform.lua
@@ -1,36 +1,78 @@
-return {
-  "stevearc/conform.nvim",
+local nvim_env = os.getenv("NVIM_ENV")
 
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-  },
-  config = function()
-    local conform = require("conform")
+if nvim_env == "work" then
+  return {
+    "stevearc/conform.nvim",
 
-    conform.setup({
-      formatters_by_ft = {
-        html = { "prettier" },
-        json = { "jq" },
-        yaml = { "prettier" },
-        xml = { "prettier" },
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
 
-    vim.keymap.set({ "n", "v" }, "<leader>ff", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          html = { "prettier" },
+          json = { "jq" },
+          yaml = { "prettier" },
+          xml = { "prettier" },
+          markdown = { "prettier" },
+          python = { "isort", "black" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
       })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
-}
+
+      vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  }
+else
+  return {
+    "stevearc/conform.nvim",
+
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          html = { "prettier" },
+          json = { "jq" },
+          yaml = { "prettier" },
+          xml = { "prettier" },
+          markdown = { "prettier" },
+          lua = { "stylua" },
+          python = { "isort", "black" },
+        },
+        format_on_save = {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>ff", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  }
+end
