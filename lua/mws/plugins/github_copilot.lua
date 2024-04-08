ebd0b19bb038d40113b17dad98134826cb098187
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2024-04-08 12:03:29 +0200

    Updates + made copilot only private.

--- lua/mws/plugins/github_copilot.lua
+++ lua/mws/plugins/github_copilot.lua
@@ -1,11 +1,20 @@
-return {
-  "github/copilot.vim",
-  config = function()
-    -- Use control + enter to accept copilot suggstions
-    vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
-}
+local nvim_env = os.getenv("NVIM_ENV")
+
+if nvim_env == "work" then
+  return {}
+else
+  return {
+    "github/copilot.vim",
+
+    branch = "release",
+
+    config = function()
+      -- Use control + enter to accept copilot suggstions
+      vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.g.copilot_no_tab_map = true
+    end,
+  }
+end
